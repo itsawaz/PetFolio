8450720bd13e7a39d6f3aae746e8cfdb11f6f1b1
Neeraj Singh <helloneerajkumarsingh@gmail.com>
2023-04-05 22:16:25 +0530

    commit

--- login/views.py
+++ login/views.py
@@ -3,21 +3,35 @@ from signup.models import Peto
 from django.contrib import messages
 from django.contrib.auth import authenticate, login
 from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+
 
 
 def login_view(request):
-    user1=Peto()
+
     if request.method == 'POST':
         email_username = request.POST['email_username']
         password = request.POST['password']
-        print(email_username,password)
-        user = authenticate(request, username=email_username, password=password)
+        #
        # if User.objects.filter(username=email_username).exists() or User.objects.filter(email=email_username).exists():
        #     if User.objects.filter(password=password).exists():
-        if user is not None:
-            login(request, user)
-            return redirect('blog-home')  # Replace 'home' with the name of your home page URL pattern
+        if User.objects.filter(username=email_username).exists():
+            user = authenticate(request, username=email_username, password=password)
+
+            if user is not None:
+                    login(request, user)
+                    return redirect('blog-home')  # Replace 'home' with the name of your home page URL pattern
+            else:
+                    messages.info(request, f'Invalid username or password')
+                    return redirect('login-home')
         else:
-            messages.info(request, f'Invalid username or password')
-            return redirect('login-home')
+            user1 = User.objects.filter(email=email_username).first()
+            user = authenticate(request, username=user1.username, password=password)
+            if user is not None:
+                    login(request, user)
+                    return redirect('blog-home')  # Replace 'home' with the name of your home page URL pattern
+            else:
+                    messages.info(request, f'Invalid username or password')
+                    return redirect('login-home')
+
     return render(request, 'login/login.html', {"title": "Login"})
